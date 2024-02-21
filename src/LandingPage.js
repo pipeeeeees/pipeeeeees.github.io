52fb7a7f6e4625ac3ce693e9c76c097c1b5c26b3
David Pipes <pipeeeeees@gmail.com>
2024-02-20 21:25:31 -0500

    initial proj

---
+++ src/LandingPage.js
@@ -0,0 +1,19 @@
+// src/LandingPage.js
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My React Test Project</h1>
+      <p>
+        Hello! My name is David and this is a test of react on github.io.
+        Here, I'll be showcasing some of the projects I've worked on... maybe someday
+      </p>
+      <p>
+        pipes out
+      </p>
+    </div>
+  );
+};
+
+export default LandingPage;
